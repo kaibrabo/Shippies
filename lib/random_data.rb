5ba86f7dd2cd757de423a4e28d922010011d8c31
Kai Brabo <kaibrabo@gmail.com>
2017-07-19 15:41:16 -0700

    7 crud intro

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,5 +1,5 @@
 module RandomData
-    # #6
+
     def self.random_paragraph
         sentences = []
         rand(4..6).times do
@@ -9,7 +9,6 @@ module RandomData
         sentences.join(" ")
     end
 
-    # #7
     def self.random_sentence
         strings = []
         rand(3..8).times do
@@ -20,7 +19,6 @@ module RandomData
         sentence.capitalize << "."
     end
 
-    # #8
     def self.random_word
         letters = ('a'..'z').to_a
         letters.shuffle!
